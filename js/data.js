32e09ae274af51279194c74d3b9360700e2b685d
zhouyoujun888 <a390687273@qq.com>
2025-11-12 15:09:54 +0800

    111

--- js/data.js
+++ js/data.js
@@ -16,5 +16,37 @@ const playlist = [
         cover: 'images/点点.jpg',
         background: 'images/点点.jpg'
     },
+    {
+        id: 3,
+        title: '梵高先生',
+        artist: '知名艺术家',
+        src: 'audio/梵高先生.mp3',
+        cover: 'images/李志1.jpg',
+        background: 'images/李志1.jpg'
+    },
+    {
+        id: 4,
+        title: '关于郑州的记忆',
+        artist: '知名艺术家',
+        src: 'audio/关于郑州的记忆.mp3',
+        cover: 'images/李志2.jpg',
+        background: 'images/李志2.jpg'
+    },
+    {
+        id: 5,
+        title: '山阴路的夏天',
+        artist: '知名艺术家',
+        src: 'audio/山阴路的夏天.mp3',
+        cover: 'images/李志3.jpg',
+        background: 'images/李志3.jpg'
+    },
+    {
+        id: 6,
+        title: '天空之城',
+        artist: '知名艺术家',
+        src: 'audio/天空之城.mp3',
+        cover: 'images/李志4.jpg',
+        background: 'images/李志4.jpg'
+    },
  
 ];
